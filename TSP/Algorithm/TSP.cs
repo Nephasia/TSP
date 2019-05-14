4f4ea70f807d596972a75085354001ee1e6eded0
Nephasia <shiverz@o2.pl>
2019-05-14 17:19:58 +0200

    no message

--- TSP/Algorithm/TSP.cs
+++ TSP/Algorithm/TSP.cs
@@ -10,124 +10,62 @@ namespace TSP.Algorithm
     class TSP
     {
 
-        Graph graph = new Graph ();
+        //Graph graph = new Graph ();
 
 
-        public int TravellingSP(Graph g, int start)
-        {
-            int min_path = Int32.MaxValue;
+        //public int TravellingSP(Graph g, int start)
+        //{
+        //    int min_path = Int32.MaxValue;
 
-            int current_path = 0;
-            int[] sciezki = new int[100];
-            for (int i = 0; i < g.Count; i++)
-            {
-                for (int j = 0; j < g.Count; j++)
-                {
+        //    int current_path = 0;
+        //    int[] sciezki = new int[100];
+        //    for (int i = 0; i < g.Count; i++)
+        //    {
+        //        for (int j = 0; j < g.Count; j++)
+        //        {
 
-                }
-            }
+        //        }
+        //    }
            
 
-        }
+        //}
 
-        int travllingSalesmanProblem(int graph[][V], int s)
-        {
-            //// store all vertex apart from source vertex 
-            //vector<int> vertex;
-            //for (int i = 0; i < V; i++)
-            //    if (i != s)
-            //        vertex.push_back(i);
+        //int travllingSalesmanProblem(int graph[][V], int s)
+        //{
+        //    //// store all vertex apart from source vertex 
+        //    //vector<int> vertex;
+        //    //for (int i = 0; i < V; i++)
+        //    //    if (i != s)
+        //    //        vertex.push_back(i);
 
-            // store minimum weight Hamiltonian Cycle. 
-            int min_path = INT_MAX;
-            do
-            {
+        //    // store minimum weight Hamiltonian Cycle. 
+        //    int min_path = INT_MAX;
+        //    do
+        //    {
 
-                // store current Path weight(cost) 
-                int current_pathweight = 0;
+        //        // store current Path weight(cost) 
+        //        int current_pathweight = 0;
 
-                // compute current path weight 
-                int k = s;
-                for (int i = 0; i < vertex.size(); i++)
-                {
-                    current_pathweight += graph[k][vertex[i]];
-                    k = vertex[i];
-                }
-                current_pathweight += graph[k][s];
+        //        // compute current path weight 
+        //        int k = s;
+        //        for (int i = 0; i < vertex.size(); i++)
+        //        {
+        //            current_pathweight += graph[k][vertex[i]];
+        //            k = vertex[i];
+        //        }
+        //        current_pathweight += graph[k][s];
 
-                // update minimum 
-                min_path = min(min_path, current_pathweight);
+        //        // update minimum 
+        //        min_path = min(min_path, current_pathweight);
 
-            } while (next_permutation(vertex.begin(), vertex.end()));
+        //    } while (next_permutation(vertex.begin(), vertex.end()));
 
-            return min_path;
-        }
-
-
-    }
-
-    class City {
-
-        public string Name { get; private set; }
-        List<Path> paths = new List<Path>();
-
-        public City(string n){
-            Name = n;
-        }
-
-        public void AddBiDirectionalPath(City to, int distance) {
-            this.AddOneDirectionPath(to, distance);
-            to.AddOneDirectionPath(this, distance);
-        }
-
-        private void AddOneDirectionPath(City to, int distance) {
-            paths.Add(new Path(to, distance));
-        }
+        //    return min_path;
+        //}
 
 
     }
 
-    class Graph {
-
-        List<City> cities = new List<City>();
-
-        public int Count => cities.Count;
-
-        public Graph this[int i] {
-            get { return cities[i]; }
-            //set { cities[i] = value; }
-        }
-
-        public void AddCity(string name) {
-            City city = new City(name);
-            cities.Add(city);
-        }
-
-        public List<City> GetCities() {
-            return cities;
-        }
-
-        public City GetCity(City city) {
-            return cities.Find(city);
-        }
-
-        public AddPath(City first, City second, int distance) {
-            cities.Find(first).AddBiDirectionalPath(cities.Find(second), distance);
-        }
-
-
-    }
-
-    class Path {
-
-        City City { get; set; }
-        public int Distance { get; set; }
-
-        public Path(City city, int distance) {
-            this.City = city;
-            this.Distance = distance;
-        }
-
-    }
+    
 
 }
---
+++ TSPConsole/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSPConsole {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+
+
+    class City {
+
+        public string Name { get; private set; }
+        List<Path> paths = new List<Path>();
+
+        public City(string n) {
+            Name = n;
+        }
+
+        public void AddBiDirectionalPath(City to, int distance) {
+            this.AddOneDirectionPath(to, distance);
+            to.AddOneDirectionPath(this, distance);
+        }
+
+        private void AddOneDirectionPath(City to, int distance) {
+            paths.Add(new Path(to, distance));
+        }
+
+
+    }
+
+    class Graph {
+
+        List<City> cities = new List<City>();
+
+        public int Count => cities.Count;
+
+        public Graph this[int i] {
+            get { return cities[i]; }
+            //set { cities[i] = value; }
+        }
+
+        public void AddCity(string name) {
+            City city = new City(name);
+            cities.Add(city);
+        }
+
+        public List<City> GetCities() {
+            return cities;
+        }
+
+        public City GetCity(City city) {
+            return cities.Find(city);
+        }
+
+        public AddPath(City first, City second, int distance) {
+            cities.Find(first).AddBiDirectionalPath(cities.Find(second), distance);
+        }
+
+
+    }
+
+    class Path {
+
+        City City { get; set; }
+        public int Distance { get; set; }
+
+        public Path(City city, int distance) {
+            this.City = city;
+            this.Distance = distance;
+        }
+
+    }
+
+}
