8ee677fc39a25e099d19c69f4ab84953afaa7897
ASUSR510J\Marcin <mo2035@wp.pl>
2019-05-14 17:12:14 +0200

    no message

--- TSP/App.xaml.cs
+++ TSP/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics;
 
 namespace TSP
 {
@@ -13,5 +14,9 @@ namespace TSP
     /// </summary>
     public partial class App : Application
     {
+        public void Func()
+        {
+            Console.WriteLine("aa");
+        }
     }
 }
